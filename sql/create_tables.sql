b866bb7650c8772fc4ca59c17cba33db8290869d
Konsta Kutvonen <djiffit@gmail.com>
2015-12-20 04:40:17 +0200

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,25 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Loggedin(
+  id SERIAL PRIMARY KEY,
+  name varchar(30) NOT NULL,
+  password varchar(30) NOT NULL
+);
+
+CREATE TABLE Thread(
+  id SERIAL PRIMARY KEY,
+  time Timestamp,
+  topic varchar(70) NOT NULL,
+  starter INTEGER REFERENCES Loggedin(id)
+);
+
+CREATE TABLE Message(
+  id SERIAL PRIMARY KEY,
+  content text,
+  time Timestamp,
+  author INTEGER REFERENCES Loggedin(id),
+  thread INTEGER REFERENCES Thread(id)
+);
+
+CREATE TABLE Replier(
+  authorid INTEGER REFERENCES Loggedin(id)
+  threadid INTEGER REFERENCES Thread(id)
+);
